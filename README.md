d083f097c4c51d4977ee629c967c2420ed5bcd38
jR <btunik555@seznam.cz>
2022-04-08 18:02:17 +0200

    Přidání manager.sh, assets, README.md

---
+++ README.md
@@ -0,0 +1,37 @@
+# ZSH For Terminal
+
+## General Information
+- Customization terminal.
+- Terminal that look like from Kali Linux
+
+## Table of contents
+- [Instalation](#Instalation)
+- [Features](#Features)
+
+## Instalation
+
+### Manager
+1. Download repo
+```
+$ git clone
+```
+2. Update permisions
+```
+$ chmod +x manager.sh
+```
+3. Run script
+```
+$ ./manager.sh
+```
+
+### Change bash > zsh
+
+#### Konsole
+1. Settings
+2. Edit current profile
+3. General > Command = usr/bin/zsh
+
+## Features
+- [x] Install zsh
+- [x] Install addon
+- [x] Install template .zshrc
---
+++ manager.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+clear
+
+echo "Vítej, poutníku."
+
+echo "Vyber z možností:"
+
+echo "     
+    0) Nainstalovat ZSH
+    1) Nakopírovat .zshrc | Z kali linuxu, ale upravený + git
+    2) Nainstalovat addons >
+"
+
+read option
+clear
+
+case $option in
+    0)
+        echo "Ok."
+        echo "Instaluji"
+        sudo apt install zsh -y
+        clear
+        if [ -f /usr/bin/zsh ]; then
+            echo "Hezkyyyy, zsh nainstalován."
+        else
+            echo "?? Ne? Prej zsh nemáš."
+        fi
+        ;;
+
+    1)
+        echo "Ok."
+        echo "Instaluji"
+        sudo apt install zsh -y
+        clear
+
+        if [ -f /usr/bin/git ]; then
+            echo "Hezkyyyy, git nainstalován."
+        else
+            echo "?? Ne? Prej git nemáš."
+        fi
+        ;;
+    
+    2)
+        echo "Co chceš nainstalovat:"
+        echo "Mám:
+        0) autosuggestions
+        1) bash-completion
+        2) git
+        "
+
+        read addon
+
+        if [ $addon == 0 ]; then
+            sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting /usr/share/zsh-syntax-highlighting
+
+            clear
+            echo "Nakopírováno."
+        fi
+
+        if [ $addon == 1 ]; then
+            sudo git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/zsh-autosuggestions
+            
+            clear
+            echo "Nakopírováno."
+        fi
+
+        if [ $addon == 2 ]; then
+            sudo apt install git -y
+
+            clear
+            
+            if [ -f /usr/bin/git ]; then
+                echo "Hezkyyyy, git nainstalován."
+            else
+                echo "?? Ne? Prej git nemáš."
+            fi
+        fi
+        ;;
+    
+    3)
+        echo "Kopíruju"
+        rm ~/.zshrc
+        cp .zshrc ~/.zshrc
+        echo "Nakopírováno."
+        ;;
+
+    *)
+        echo "Musíš vybrat z možností ne si je vymyslet"
+        ;;
+    
+esac
+
+echo
+echo "Užívej"
